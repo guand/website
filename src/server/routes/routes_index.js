e144ac15ec5e159ee0ab6818f7a38e644e60fb5c
Jian Guan <jianguan@Jians-MBP-2.lan>
2018-11-19 01:05:17 -0800

    Added blog site

---
+++ src/server/routes/routes_index.js
@@ -0,0 +1,26 @@
+
+// Export our app routes
+exports = module.exports = function (app) {
+  // Set up the default app route to  http://localhost:3000/index.html
+  app.get('/index.html', function (req, res) {
+    // Render some simple boilerplate html
+    function renderFullPage() {
+   // Note the div class name here, we will use that as a hook for our React code
+      return `
+		<!doctype html>
+		<html>
+			<head>
+				<title>Keystone With React And Redux</title>
+			</head>
+			<body>
+        			<div class="react-container">
+					<h1> Hello World </h1>
+        			</div>
+			</body>
+		</html>
+		`;
+    }
+    // Send the html boilerplate
+    res.send(renderFullPage())
+  })
+}
---
+++ src/updates/0.0.1-admin.js
@@ -0,0 +1,11 @@
+import keystone from 'keystone'
+var User = keystone.list('User')
+
+exports = module.exports = function (done) {
+	new User.model({
+		name: { first: 'admin', last: 'user' },
+		email: 'admin@keystonejs.com',
+		password: 'admin',
+		canAccessKeystone: true,
+	}).save(done)
+}
